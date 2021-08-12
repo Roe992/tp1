329ca3bd0a834a2ea31e991e7fc799a57a504dd4
Roe992 <manugrassi9@gmail.com>
2021-08-12 09:01:00 -0300

    primerIteracion

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int arreglo[10];
+    printf("Hello world!\n");
+    return 0;
+}
+
+
+int cargarArray (int arreglo[], int dimension)
+{
+    int i;
+for (i=0; i<dimension;i++){
+    arreglo[i]=-1;
+
+}
+return i;
+}
